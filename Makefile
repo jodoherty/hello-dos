84fc7853d7dcbe53cc54f2a6f727236e6725a9ef
James O'Doherty <james@odoherty.family>
2021-05-08 23:15:04 -0400

    Use real mode DOS services.

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 CC=i586-pc-msdosdjgpp-gcc
 LD=i586-pc-msdosdjgpp-ld
 AS=i586-pc-msdosdjgpp-as
-CFLAGS=-Wall -pedantic -std=c17
+CFLAGS=-Wall -pedantic -std=gnu17
 
 all: hello.exe
 .PHONY: all
+
+clean:
+	$(RM) hello.exe *.o
+.PHONY: clean
 
 hello.exe: hello.o
 	$(CC) -o $@ $^
--- hello.c
+++ hello.c
@@ -1,10 +1,25 @@
-#include <stdio.h>
+#include <dpmi.h>
+
+static void myputc(char ch)
+{
+  __dpmi_regs r;
+  r.h.ah = 0x02;
+  r.h.dl = ch;
+  __dpmi_int(0x21, &r);
+}
+
+static void myputs(const char *s) {
+  while (*s) {
+    myputc(*s);
+    s++;
+  }
+}
 
 int main()
 {
-  printf("Hello, World\n");
-  for (int i=0; i<100; i++) {
-    printf("%d\n", i);
-  }
+  const char *s = "Hello, World\n";
+  myputs(s);
+  myputs(s);
+  myputs(s);
   return 0;
 }
